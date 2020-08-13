0145c4d65350ecddbf0d292e7339b3dc456f4cac
Glitch (jira-issue-to-new-branch) <none>
2020-08-13 09:39:33 +0000

    🎢🐈 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -3,70 +3,71 @@
 
 console.log('hello world :o');
 
-// our default array of dreams
-const dreams = [
-];
+// our default array of issues
+const issues = [];
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const issueIdInput = dreamsForm.elements['issue-id'];
-const dreamInput = dreamsForm.elements['dream'];
+const issuesList = document.getElementById('issues');
+const issuesForm = document.forms[0];
+const issueIdInput = issuesForm.elements['issue-id'];
+const issueInput = issuesForm.elements['issue'];
 
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
+// a helper function that creates a list item for a given issue
+const appendNewIssue = function(issue) {
+    const newListItem = document.createElement('li');
+    newListItem.innerHTML = issue;
+    issuesList.appendChild(newListItem);
 }
 
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
+// iterate through every issue and add it to our page
+issues.forEach( function(issue) {
+    appendNewIssue(issue);
 });
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
+// listen for the form to be submitted and add a new issue when it is
+issuesForm.onsubmit = function(event) {
+    // stop our form submission from refreshing the page
+    event.preventDefault();
 
-  // get dream value and add it to the list
-  var spacesRegex = / /g;
-  var dashesRegex = /(\B-)+/g;
-  
-  var issueId = issueIdInput.value;
-  var dream = dreamInput.value.toLowerCase();
-  
-  dream = dream.replace(spacesRegex, "-");
-  dream = dream.replace("->", "-");
-  dream = dream.replace("_", "-");
-  dream = dream.replace("(", "-");
-  dream = dream.replace(")", "-");
-  dream = dream.replace("[be]", "");
-  // replace several - by a single one
-  dream = dream.replace(dashesRegex, "");
-  dream = "git checkout -b " + issueId + "-" + dream.replace("[be]", "");
-  dreams.push(dream);
-  appendNewDream(dream);
-  
-//   var issueId = issueIdInput.value;
-//   var dream = dreamInput.value.toLowerCase();
-  
-//   dream = issueId + " " + dream;
-//   dreams.push(dream);
-//   appendNewDream(dream);
-  
-  var issueTitlePartRegex = /((.*)->\s)(.*)/g;
-  var issueTitlePart = issueTitlePartRegex.exec(dreamInput.value)[3];
-  
-  dream = issueId + " " + issueTitlePart;
-  dreams.push(dream);
-  appendNewDream(dream);
-  
-  dream = issueTitlePart;
-  dreams.push(dream);
-  appendNewDream(dream);
+    // get issue value and add it to the list
+    var spacesRegex = / /g;
+    var dashesRegex = /(\B-)+/g;
+    var quotesRegex = /"/g;
+//   labels are in the form [*]
+    var labelsRegex = /\[.*\]/gm;
 
-  // reset form 
-  dreamInput.focus();
+    var issueId = issueIdInput.value;
+    var issue = issueInput.value.toLowerCase();
+
+    issue = issue.replace(quotesRegex, "");
+    issue = issue.replace(spacesRegex, "-");
+    issue = issue.replace(labelsRegex, "");
+
+    issue = issue.replace("->", "-");
+    issue = issue.replace("_", "-");
+    issue = issue.replace("(", "-");
+    issue = issue.replace(")", "-");
+    issue = issue.replace("\'", "");
+
+    var issueGitClone = "git checkout -b " + issueId + "-" + issue;
+
+    // replace several `-`` with a single one
+    issueGitClone = issueGitClone.replace(dashesRegex, "");
+
+    issues.push(issueGitClone);
+    appendNewIssue(issueGitClone);
+
+    // var issueTitlePartRegex = /((.*)->\s)(.*)/g;
+    var issueTitle = issueInput.value.replace(labelsRegex, "");
+
+    issue = issueId + " - " + issueTitle;
+    issues.push(issue);
+    appendNewIssue(issue);
+
+    issue = issueTitle;
+    issues.push(issue);
+    appendNewIssue(issue);
+
+    // reset form
+    issueInput.focus();
 };
